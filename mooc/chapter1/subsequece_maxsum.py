4af3ae24061863419e04f5343b82d675e8464538
rurui <rurui739759848@live.com>
2018-10-22 23:25:59 +0800

    submax

--- mooc/chapter1/subsequece_maxsum.py
+++ mooc/chapter1/subsequece_maxsum.py
@@ -1,14 +1,33 @@
-def aaa():
+def maxC2(ls, low, upp):
+    "divide and conquer"
+    if ls is None:
+        return 0
+    elif low == upp:
+        return ls[low]
 
-    chn = ('ling', 'yi', 'er', 'san', 'si', 'wu', 'liu', 'qi', 'ba', 'jiu')
+    mid = (low + upp) // 2  # notice: in the higher version python, “/” would get the real value
+    lmax = 0
+    rmax = 0
+    tmp = 0
+    i = mid
+    while i >= low:
+        tmp += ls[i]
+        if tmp > lmax:
+            lmax = tmp
+        i -= 1
+    tmp = 0
+    for k in range(mid + 1, upp):
+        tmp += ls[k]
+        if tmp > rmax:
+            rmax = tmp
+    return max3(rmax + lmax, maxC2(ls, low, mid), maxC2(ls, mid + 1, upp))
 
-    num = input()
-    sum = 0
-    out_sum = ''
 
-    for i in num:
-        sum += int(i)
-    for j in str(sum):
-        out_sum += chn[int(j)] + ' '
-    print(out_sum.rstrip())
-aaa()
+def max3(x, y, z):
+    if x >= y and x >= z:
+        return x
+    return max3(y, z, x)
+
+
+value = maxC2([3, -1, ], 0, 1)
+print(value)
